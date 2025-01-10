dc51aab91e5b549f8e44c6e4949d2b79b9198787
Victor Brouwer <victor.brouwer@hotmail.com>
2025-01-10 17:55:14 +0100

    changed bio

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,7 +7,7 @@ const About = () => {
         <h2 className="text-4xl font-bold text-center mb-8">About Me</h2>
         <p className="text-black mb-6">
         I'm a data scientist and developer specializing in machine learning, statistical analysis, and software development. My work focuses on transforming complex data into meaningful insights and building effective solutions.
-        Outside of work, I'm an avid sports enthusiast. You can find me following Ajax football matches, playing tennis, improving my golf game, or kitesurfing when conditions are right. I also enjoy spending time woodworking and reading to unwind.
+        Outside of work, I'm an avid sports enthusiast. You can find me following Ajax football matches, playing tennis, improving my golf game, or kitesurfing when conditions are right. I also enjoy spending time woodworking, reading and listening to podcasts to unwind.
         </p>
         
       </div>
